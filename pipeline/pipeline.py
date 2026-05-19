09902de6f2fac0014caaacc291c55bdbcf0bf6fe
Tiago André Barbosa Paiva <63262154+barbosapaiva@users.noreply.github.com>
2026-05-19 19:11:50 +0000

    pipeline code working

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 print("arguments", sys.argv)
 
-day = int(sys.argv[1])
-print(f"Running pipeline for day {day}")
+month = int(sys.argv[1])
+print(f"Running pipeline for month {month}")
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
 print(df.head())
 
-df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+df.to_parquet(f"output_month_{sys.argv[1]}.parquet")
